066cfdd7c783aecd9a300d92155bd1fb17b9b273
Grace Gardner <graceirisgardner@gmail.com>
2021-11-29 22:16:12 -0700

    Update test

--- test/turn-test.js
+++ test/turn-test.js
@@ -5,6 +5,16 @@ const Turn = require('../src/turn');
 const Card = require('../src/Card')
 
 describe('Turn', function() {
+  let card;
+  let turn;
+
+  beforeEach(() => {
+    card = new Card(123, "Is this a question?", ["yes", "no"], "yes");
+  })
+
+  beforeEach(() => {
+    turn = new Turn("yes", card);
+  })
 
   it('should be a function', function() {
     const turn = new Turn();
@@ -17,43 +27,31 @@ describe('Turn', function() {
   })
 
   it('should hold a card', function() {
-    const card = new Card(123, "Is this a question?", ["yes", "no"], "yes");
-    const turn = new Turn("yes", card);
     expect(turn.card.correctAnswer).to.equal("yes");
   })
 
   it('should be able to return a guess', function() {
-    const card = new Card(123, "Is this a question?", ["yes", "no"], "yes");
-    const turn = new Turn("yes", card);
     expect(turn.returnGuess()).to.equal("yes");
   })
 
   it('should return a card', function() {
-    const card = new Card(123, "Is this a question?", ["yes", "no"], "yes");
-    const turn = new Turn("yes", card);
     expect(turn.returnCard()).to.equal(card);
   })
 
-  it.skip('should evaluate a correct guess', function() {
-    const card = new Card(123, "Is this a question?", ["yes", "no"], "yes");
-    const turn = new Turn("yes", card);
+  it('should evaluate a correct guess', function() {
     expect(turn.evaluateGuess()).to.equal(true);
   })
 
   it.skip('should evaluate a incorrect guess', function() {
-    const card = new Card(123, "Is this a question?", ["yes", "no"], "yes");
     const turn = new Turn("no", card);
     expect(turn.evaluateGuess()).to.equal(false);
   })
 
   it.skip('should give feedback for correct answers', function() {
-    const card = new Card(123, "Is this a question?", ["yes", "no"], "yes");
-    const turn = new Turn("yes", card);
     expect(turn.giveFeedback()).to.equal("correct!")
   })
 
   it.skip('should give feedback for incorrect answers', function() {
-    const card = new Card(123, "Is this a question?", ["yes", "no"], "yes");
     const turn = new Turn("no", card);
     expect(turn.giveFeedback()).to.equal("incorrect!");
   })
