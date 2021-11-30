7faae39885a4584cd2c708b1535307d0fe49f788
Grace Gardner <graceirisgardner@gmail.com>
2021-11-29 22:54:53 -0700

    Stop for the night

--- src/turn.js
+++ src/turn.js
@@ -11,6 +11,16 @@ class Turn {
   returnCard() {
     return this.card;
   }
+
+  evaluateGuess() {
+    switch(this.guess) {
+      case this.card.correctAnswer:
+      return true;
+        break;
+      default:
+      return false;
+    }
+  }
 }
 
 module.exports = Turn;
