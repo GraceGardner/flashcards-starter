4ba7cf4701770e19da0359575b6335ccfb917ca3
Grace Gardner <graceirisgardner@gmail.com>
2021-11-30 19:14:31 -0700

    Finish the deck test

--- test/deck-test.js
+++ test/deck-test.js
@@ -1,7 +1,6 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-// const prototypeData = require('../src/data');
 const Card = require('../src/Card');
 const Deck = require('../src/deck');
 
@@ -18,32 +17,17 @@ describe('Deck', () => {
   })
 
   it('should be a function', () => {
-    const deck = new Deck(cards)
+    const deck = new Deck(cards);
     expect(Deck).to.be.a('function');
   })
 
+  it('should take a array of cards', () => {
+    const deck = new Deck(cards);
+    expect(deck.cards[0]).to.be.an.instanceof(Card);
+  })
+
+  it('should count cards', () => {
+    const deck = new Deck(cards);
+    expect(deck.countCards()).to.equal(5);
+  })
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Your Deck class should be initialized with an array of Card objects and should have an accompanying test file. It should know how many Cards are in the Deck.
-
-// const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-// const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-// const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-//
-// const deck = new Deck([card1, card2, card3]);
-//
-// deck.countCards(); // => 3
