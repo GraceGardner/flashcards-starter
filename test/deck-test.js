11b7dc0e3d80809cf05a663a3270cbf5f156387c
Grace Gardner <graceirisgardner@gmail.com>
2021-11-30 18:52:32 -0700

    Create deck file

--- test/deck-test.js
+++ test/deck-test.js
@@ -9,16 +9,17 @@ describe('Deck', () => {
   let cards;
 
   beforeEach(() => {
-    cardOne = new Card(123, "Is this a question?", ["yes", "no"], "yes");
-    cardTwo = new Card(124, "Is this a question 2?", ["yes", "no"], "yes");
-    cardThree = new Card(125, "Is this not a question 3?", ["yes", "no"], "no");
-    cardFour = new Card(126, "Is this a question 4?", ["yes", "no"], "yes");
-    cardFive = new Card(127, "Is this not a question 5?", ["yes", "no"], "no");
+    const cardOne = new Card(123, "Is this a question?", ["yes", "no"], "yes");
+    const cardTwo = new Card(124, "Is this a question 2?", ["yes", "no"], "yes");
+    const cardThree = new Card(125, "Is this not a question 3?", ["yes", "no"], "no");
+    const cardFour = new Card(126, "Is this a question 4?", ["yes", "no"], "yes");
+    const cardFive = new Card(127, "Is this not a question 5?", ["yes", "no"], "no");
     cards = [cardOne, cardTwo, cardThree, cardFour, cardFive];
   })
 
   it('should be a function', () => {
-    const deck = new
+    const deck = new Deck(cards)
+    expect(Deck).to.be.a('function');
   })
 
 })
