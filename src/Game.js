e95a992e47ef42d104ed79ea191ae08290f1eb34
Grace Gardner <graceirisgardner@gmail.com>
2021-12-02 16:01:44 -0700

    Complete game class

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,26 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Turn = require('../src/turn');
+const Card = require('../src/Card');
+const Round = require('../src/round');
+const Deck = require('../src/deck');
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.cards = [];
+    this.deck = undefined;
+    this.round = undefined;
+  }
+
+  start() {
+    let cards = prototypeQuestions.map(card =>
+      new Card(card.id, card.question, card.answers, card.correctAnswer))
+    this.deck = new Deck(cards);
+    this.round = new Round(this.deck);
+    this.printMessage(this.deck, this.round);
+    this.printQuestion(this.round);
+  }
 
   printMessage(deck, round) {
     console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
--- test/Game-test.js
+++ test/Game-test.js
@@ -1,20 +1,42 @@
+const chai = require('chai');
+const expect = chai.expect;
 
-// Playing the Game
-//
-// Now that we have all of our classes and methods set up, we can connect this to the pre-existing code to make the game playable through the CLI (Command-Line-Interface).
-//
-// Game
-//
-// As you may have noticed, your Game class has two methods fleshed out already: printMessage and printQuestion. We are going to abandon testing for these methods - as the techniques for this type of testing are beyond the scope of this project.
-//
-// Your Game class should meet these other requirements:
-// Should keep track of the currentRound
-// start: method that starts everything
-// Creates Cards
-// Puts Cards in a Deck
-// Creates a new Round using the Deck
-// invokes printMessage to display the message in the CLI
-// invokes printQuestion to kick off our helper functions that allow interaction via the CLI
-// Note: The helper functions are all fleshed out and fit to work with classes/methods that meet the requirements in the past iterations.
-//
-// Look at the file being run when we want to start the game. Think about where you need to invoke your Game.start method.
+const Turn = require('../src/turn');
+const Card = require('../src/Card');
+const Round = require('../src/round');
+const Deck = require('../src/deck');
+const Game = require('../src/Game')
+const data = require('../src/data');
+const prototypeQuestions = data.prototypeData;
+
+describe('Game', () => {
+  let game;
+
+  beforeEach(() => {
+    game = new Game();
+  })
+
+  it('should keep track of round', () => {
+    expect(game.round).to.deep.equal(undefined)
+  })
+
+  it('should have a start method', () =>{
+    expect(game.start).to.be.a('function')
+  })
+
+  it('should create cards', () =>{
+    game.start();
+    expect(game.deck.cards[0]).to.be.an.instanceof(Card);
+  })
+
+  it('should have a deck', () =>{
+    game.start()
+    expect(game.deck).to.be.an.instanceof(Deck);
+  })
+
+  it('should create a new round', () =>{
+    game.start()
+    expect(game.round).to.be.an.instanceof(Round);
+  })
+
+})
